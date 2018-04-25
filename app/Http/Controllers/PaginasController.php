936ec451335681e257c60bab029f4f181ac47ebd
Gabriel <gabrielantoniocp9@gmail.com>
2018-04-24 21:08:54 -0300

    Adicionado composer e Pug ao sistema

--- app/Http/Controllers/PaginasController.php
+++ app/Http/Controllers/PaginasController.php
@@ -15,17 +15,17 @@ class PaginasController extends Controller {
 
     $nomeCompleto = $nome . " " . $sobrenome;
     $email = 'gabrielantoniocp9@gmail.com';
-    $dados = array (
-
-        'email' => $email,
-        'nome'  => $nomeCompleto
-
-    );
+    // $dados = array (
+    //
+    //     'email' => $email,
+    //     'nome'  => $nomeCompleto
+    //
+    // );
     // Uma maneira de inserir informações no View
-    //return view('paginas.sobre')->withNomeCompleto($nomeCompleto)->withEmail($email);
+    return view('paginas.sobre')->withNomeCompleto($nomeCompleto)->withEmail($email);
 
-    // Passando informações de um array para o view;
-    return view('paginas.sobre')->withDados($dados);
+    // Passando informações de um array para o view
+    // return view('paginas.sobre')->withDados($dados);
   }
 
   public function getContato() {
